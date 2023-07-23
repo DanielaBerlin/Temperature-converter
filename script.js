93f8b2cb11104a3a6235e6b9ad30fe163d46ce10
Daniela <danielacardona2203@gmail.com>
2023-07-23 13:59:10 +0200

    Add temperature conversion function

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-// --Convert Celsius to--
-// fah = (x * 1.8) + 32;
-// kelvin = x + 273.15;
+const celsius = document.querySelector(".celsius"),
+    fah = document.querySelector(".fahrenheit"),
+    kelvin = document.querySelector(".kelvin"),
+    form = document.querySelector("form");
 
-// -- Convert Fah to --
-// celsius = (x - 32) / 1.8;
-// kelvin = (x-32) / 1.8 + 273.15;
+form.addEventListener("input", convertTemperature);
 
-// -- Convert Kelvin to --
-//fah = (x - 273.15) * 1.8 + 32;
-// celsius = parseFloat(x) - 273.15;
+function convertTemperature(e) {
+    if (e.target.classList.contains("celsius")) {
+        let x = e.target.value;
+        fah.value = (x * 1.8) + 32;
+        kelvin.value = x + 273.15;
+
+    }
+    if (e.target.classList.contains("fahrenheit")) {
+        let x = e.target.value;
+        celsius.value = (x - 32) / 1.8;
+        kelvin.value = (x - 32) / 1.8 + 273.15;
+
+    }
+    if (e.target.classList.contains("kelvin")) {
+        let x = e.target.value;
+        fah.value = (x - 273.15) * 1.8 + 32;
+        celsius.value = parseFloat(x) - 273.15;
+
+    }
+}
